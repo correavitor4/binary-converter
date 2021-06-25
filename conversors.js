5772684321bba11d3e061b6320c00c8317af23e7
João Vitor Corrêa <4correavitor@gmail.com>
2021-06-25 14:04:41 -0300

    Adicionei uma função que converte de binário para decimal e outra que converte de binário para octal

--- conversors.js
+++ conversors.js
@@ -1,4 +1,5 @@
 //Os parâmetros dessa função devem ser fornecidos em formato de string com algarismos variando de 0 a 9
+//Ela retorna uma string
 function decimalToBinary( decimal_number_received){
     let decimal_number=parseInt(decimal_number_received)
     let binary=[]
@@ -34,6 +35,7 @@ function decimalToBinary( decimal_number_received){
 }
 
 //Os parâmetros dessa função devem ser fornecidos em formato de string, com algarismos variando de 0 a 7
+//Ela retorna uma string
 function octalToBinary(octal_number_received){
     let octal = octal_number_received.split("")
     let binary=[]
@@ -98,6 +100,7 @@ function octalToBinary(octal_number_received){
 }
 
 //Os parâmetros dessa função devem ser fornecidos em formato de string, e com "letras" maiúsculas e algarismos numéricos variando entre 0 e 9
+//Ela retorna uma string
 function hexadecimalToBinary(hexadecimal_number_received){
     let hexadecimal = hexadecimal_number_received.split("")
     let binary = []
@@ -213,6 +216,88 @@ function hexadecimalToBinary(hexadecimal_number_received){
     return binary_string
 }
 
+//Os parâmetros dessa função devem ser fornecidos em formato de string, contendo apenas 0 e 1 como algarismos
+//Ela retorna uma string
+function binaryToDecimal(binary_number_received){
+    let binary = binary_number_received.split("")
+    let inverted_binary=binary.reverse()
+    let decimal =0
+    let decimal_string
 
+    for(let i=0;i<inverted_binary.length;i++){
+        decimal += parseInt(inverted_binary[i])*Math.pow(2,i)      
+    }
+
+    decimal_string=decimal.toString()
+
+    return decimal_string
+
+    
+
+
+}
+
+//Os parâmetros dessa função devem ser fornecidos em formato de string, contendo apenas 0 e 1 como algarismos
+//Ela retorna uma string
+function binaryToOctal(binary_number_received){
+    let binary = binary_number_received.split("")
+    let binary_ready_for_conversion = []
+    let octal=[]
+    let octal_string=""
+
+    while(binary.length<3){
+        binary.unshift("0")
+    }
+
+    while(binary.length%3 !=0){
+        binary.unshift("0")
+    }
+
+    
+
+    for(let i=0;i< binary.length;i+=3){
+        binary_ready_for_conversion.push(binary[i]+binary[i+1]+binary[i+2])
+    }
+
+
+    for(i=0;i<binary_ready_for_conversion.length;i++){
+        switch(binary_ready_for_conversion[i]){
+            case "000":
+                octal.push("0")
+                break
+            case "001":
+                octal.push("1")
+                break
+            case "010":
+                octal.push("2")
+                break
+            case "011":
+                octal.push("3")
+                break
+            case "100":
+                octal.push("4")
+                break
+            case "101":
+                octal.push("5")
+                break
+            case "110":
+                octal.push("6")
+                break
+            case "111":
+                octal.push("7")
+                break
+            
+            }
+    }
+
+    while (octal[0]=="0") {
+        octal.shift()
+    }
+
+    octal_string=octal.join("")
+    
+    return octal_string
+
+}
 
 
